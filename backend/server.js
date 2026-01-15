086a22e815f9cbe5c89bbbfcae040db5f0fce419
Debabrata Das <debabratadas711@gmail.com>
2026-01-15 18:45:41 +0530

    debug fix

--- backend/server.js
+++ backend/server.js
@@ -10,8 +10,10 @@ import { connectDB } from "./lib/db.js";
 import { ENV } from "./lib/env.js";
 import { app, server } from "./lib/socket.js";
 
+dotenv.config();
 
 const PORT = ENV.PORT || 5000;
+const __dirname = path.resolve();
 
 // Middleware
 app.use(express.json({ limit: "5mb" }));
@@ -19,7 +21,7 @@ app.use(cookieParser());
 
 app.use(
     cors({
-        origin: ENV.CLIENT_URL,
+        origin: process.env.CLIENT_URL,
         credentials: true,
     })
 );
